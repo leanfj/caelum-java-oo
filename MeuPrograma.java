086946d5d51582f29f3853feb33a38e2af8944c3
leanfj <leanfj@hotmail.com>
2021-03-21 19:05:23 -0300

    Primeiro programa

---
+++ MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo cruel!!");
+        System.out.println("Mais uma lina usando println");
+        System.out.println("Essa é uma linha \n e essa é outra linha");
+    }
+}
