f356e05fe92c3b1175887c6c8c40ae82544faa99
leanfj <leanfj@hotmail.com>
2021-03-24 17:17:29 -0300

    Casting

---
+++ Casting.java
@@ -0,0 +1,18 @@
+public class Casting {
+    public static void main(String[] args) {
+        double d3 = 3.14;
+
+        int i = (int) d3;
+        
+        System.out.println(i);
+
+        long l4 = 10000;
+
+        int novoI = (int) l4;
+
+        System.out.println(novoI);
+
+
+    }
+    
+}
