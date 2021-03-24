da2dd86936c1a433a2504f1915fec7ceb01d8947
leanfj <leanfj@hotmail.com>
2021-03-24 16:29:53 -0300

    Utilizando variáveis

---
+++ TesteIdade.java
@@ -0,0 +1,15 @@
+public class TesteIdade {
+    public static void main (String[] args ) {
+
+        int idade = 20;
+
+        System.out.println(idade);
+
+        int idadeAnoQueVem;
+
+        idadeAnoQueVem = idade + 1;
+
+        System.out.println(idadeAnoQueVem);
+
+    }
+}
