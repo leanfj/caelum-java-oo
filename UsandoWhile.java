f10cfccc4454c30467823b7d37ce0127dd2ebdd9
leanfj <leanfj@hotmail.com>
2021-03-25 06:51:37 -0300

    Usando while

---
+++ UsandoWhile.java
@@ -0,0 +1,10 @@
+public class UsandoWhile {
+    public static void main(String[] args) {
+        int inicio = 0;
+
+        while (inicio < 10) {
+            System.out.println(inicio);
+            inicio = inicio + 1;
+        }
+    }
+}
